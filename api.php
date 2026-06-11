57fedce4cf15345da004cd1e89a3cbcf0ee6e1d5
Juhid2410 <juhid2410@gmail.com>
2026-06-11 17:28:48 +0100

    Fix 500 error in API proxy due to missing getallheaders function in PHP-FPM

--- api.php
+++ api.php
@@ -27,6 +27,18 @@ if ($input) {
     curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
 }
 
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 $headers = [];
 foreach (getallheaders() as $name => $value) {
     if (strtolower($name) !== 'host') {
